9899dc95400e2b38cdc7b2f7d570b73e82a1459f
Guilherme Souza <ghostgui60@gmail.com>
2025-02-11 22:56:24 -0300

    feat: Add buttons with leagues to select

--- src/ui/LeagueSwitcher.jsx
+++ src/ui/LeagueSwitcher.jsx
@@ -1,14 +1,32 @@
 import Button from "./Button";
+import { usePlayers } from "../context/PlayerContext";
+import { useState } from "react";
+
+const leagues = [
+    { id: 39, name: "Premier League" },
+    { id: 140, name: "La Liga" },
+    { id: 135, name: "Serie A" },
+    { id: 78, name: "Bundesliga" },
+    { id: 2, name: "UEFA Champions League" },
+];
 
 const LeagueSwitcher = () => {
+    const { selectedLeague, setSelectedLeague } = usePlayers();
+
+    const handleLeagueChange = (id) => {
+        setSelectedLeague(id);
+    };
+
     return (
-        <section className="py-4 border-b border-[#434343]/50">
-            <div className="flex items-start justify-around px-4">
-                <Button league="Premier League" />
-                <Button league="La Liga" />
-                <Button league="Bundesliga" />
-                <Button league="Uefa Champions League" />
-            </div>
+        <section className="py-4 px-4 border-b border-[#434343]/50 flex flex-wrap justify-around">
+            {leagues.map((league) => (
+                <Button
+                    key={league.id}
+                    league={league.name}
+                    onClick={() => handleLeagueChange(league.id)}
+                    isActive={selectedLeague === league.id}
+                />
+            ))}
         </section>
     );
 };
