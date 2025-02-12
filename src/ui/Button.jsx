67842b6a47be8e4a6edbc345309b4b1d3dd6653d
Guilherme Souza <ghostgui60@gmail.com>
2025-02-11 22:53:42 -0300

    feat: Creating ui of  button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,5 +1,14 @@
-const Button = ({ league }) => {
-    return <button className="w-64 h-12 bg-[#1b1d30] rounded">{league}</button>;
+const Button = ({ league, onClick, isActive }) => {
+    return (
+        <button
+            className={`px-4 py-2  mb-4 mr-4 cursor-pointer w-60 h-12  rounded transition-all ${
+                isActive ? "bg-[#4627b0] text-white" : "bg-[#1b1d30] text-white"
+            }`}
+            onClick={onClick}
+        >
+            {league}
+        </button>
+    );
 };
 
 export default Button;
