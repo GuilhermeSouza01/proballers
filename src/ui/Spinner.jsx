87d0d182c6fc380a1dbc6acfd70b50aa332ac4ff
Guilherme Souza <ghostgui60@gmail.com>
2025-02-11 22:55:32 -0300

    feat: Create a spinner

---
+++ src/ui/Spinner.jsx
@@ -0,0 +1,7 @@
+const Spinner = () => {
+    return (
+        <div className="w-16 h-16 border-4 border-t-transparent border-indigo-500 rounded-full animate-spin mx-auto my-12"></div>
+    );
+};
+
+export default Spinner;
