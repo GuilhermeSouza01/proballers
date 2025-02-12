569dda6f838782a06465c446898c4df229fba1c1
Guilherme Souza <ghostgui60@gmail.com>
2025-02-11 22:51:46 -0300

    feat: Add function to call players by league

--- src/services/apiPlayers.js
+++ src/services/apiPlayers.js
@@ -3,10 +3,22 @@ import api from "./api";
 export const fetchPlayer = async (playerId) => {
     try {
         const response = await api.get(`/players/profiles?player=${playerId}`);
-        console.log(response.data);
         return response.data;
     } catch (error) {
         console.error("Erro ao buscar jogador:", error);
         return null;
+    }
+};
+
+export const fetchPlayersByLeague = async (leagueId) => {
+    try {
+        const response = await api.get(
+            `/players?league=${leagueId}&season=2023`
+        );
+        console.log(response.data.response);
+        return response.data.response;
+    } catch (error) {
+        console.error("Erro ao buscar jogadores:", error);
+        return [];
     }
 };
