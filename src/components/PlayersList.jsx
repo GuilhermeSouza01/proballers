6b79dac2fe5faba3b3dee142b01a98b2556541a7
Guilherme Souza <ghostgui60@gmail.com>
2025-02-11 22:52:50 -0300

    feat: Add map to show players from api

--- src/components/PlayersList.jsx
+++ src/components/PlayersList.jsx
@@ -1,16 +1,30 @@
+import { useEffect } from "react";
+import { usePlayers } from "../context/PlayerContext";
 import Heading from "../ui/Heading";
+import PlayerCard from "../ui/PlayerCard";
+import Spinner from "../ui/Spinner";
 
 const PlayersList = () => {
+    const { players, loading } = usePlayers();
+
+    if (loading) {
+        return <Spinner />;
+    }
+
+    if (players.length === 0) {
+        return <div className="text-white text-center">No players found.</div>;
+    }
+
     return (
         <div className="mt-4 ml-8">
             <Heading title="Players" />
             <div className="flex flex-wrap gap-5 justify-start">
-                <div className="w-44 h-44 bg-[#1B1D30] rounded-lg"></div>
-                <div className="w-44 h-44 bg-[#1B1D30] rounded-lg"></div>
-                <div className="w-44 h-44 bg-[#1B1D30] rounded-lg"></div>
-                <div className="w-44 h-44 bg-[#1B1D30] rounded-lg"></div>
-                <div className="w-44 h-44 bg-[#1B1D30] rounded-lg"></div>
-                <div className="w-44 h-44 bg-[#1B1D30] rounded-lg"></div>
+                {players.map((playerData) => (
+                    <PlayerCard
+                        key={playerData.player.id}
+                        player={playerData.player}
+                    />
+                ))}
             </div>
         </div>
     );
